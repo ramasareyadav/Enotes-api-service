434bbcaf72fcc1e5f113315bd7282e1ae73a62d6
Ramasare Yadav <yadavrama096@gmail.com>
2025-12-24 14:31:43 +0530

    finish save category

--- src/main/java/com/enotes/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/enotes/service/impl/CategoryServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import java.util.Date;
 import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -16,6 +17,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Boolean saveCategory(Category category) {
         category.setIsDeleted(false);
+        category.setCreatedOn(new Date());
         Category saveCategory = categoryRepository.save(category);
         if(ObjectUtils.isEmpty(saveCategory))
         {
